b8b4d3d9550c557d8848cd86ba746e02870a2966
hslee15 <lhs01019@kbu.ac.kr>
2025-12-29 10:13:27 +0900

    test

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hello_1 = require("./hello");
+(0, hello_1.hello)();
+var a = 20;
+console.log("hello typescript!");
+var func = function () { return console.log("Hello"); };
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { hello } from "./hello"
+
+hello()
+
+let a:number=20
+console.log("hello typescript!")
+
+const func=()=>console.log("Hello")
